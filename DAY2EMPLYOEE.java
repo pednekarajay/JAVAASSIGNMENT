acc85f2320407b2554247041d4310130b9cd3474
ajay pednekar <68737240+pednekarajay@users.noreply.github.com>
2020-11-01 06:38:58 -0800

    Add files via upload

---
+++ DAY2EMPLYOEE.java
@@ -0,0 +1,13 @@
+package com.Employee;
+
+public class Employee {
+    int age;
+    String name;
+    String city;
+    public void display(){
+        System.out.println("The name is"+ " "+name);
+        System.out.println("The age is"+" "+age);
+        System.out.println("The City is"+" "+city);
+    }
+
+}
